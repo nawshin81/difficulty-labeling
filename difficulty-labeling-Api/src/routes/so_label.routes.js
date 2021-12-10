20b33d77a77290632c01a6495e9b3263ad2853a9
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-12-10 22:06:15 +0600

    removed api

--- difficulty-labeling-Api/src/routes/so_label.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-	const so_label = require("../controllers/so_label.controller.js");
-	var router = require("express").Router();
-
-	router.post("/", so_label.create);
-
-	app.use("/api/label", router);
-};
--- difficulty-labeling-Api/src/routes/so_question.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const so_question = require("../controllers/so_question.controller.js");
-    var router = require("express").Router();
-
-    router.get("/", so_question.findAll);
-
-    app.use('/api/questions', router);
-};
