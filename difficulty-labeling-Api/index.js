8505ab1cb7251de40cb3b0fd4ad5ee979009f9b9
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-12-10 15:49:23 +0600

    so_label insert endpoint created

--- difficulty-labeling-Api/index.js
+++ difficulty-labeling-Api/index.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 });
 
 require("./src/routes/so_question.routes")(app);
+require("./src/routes/so_label.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
