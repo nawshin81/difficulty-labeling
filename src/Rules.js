e3762d0c758c3c593d90fb8940995ffab9a5269a
nawshin81 <nawshinulfat@iut-dhaka.edu>
2021-12-18 18:18:10 +0600

    changes

--- src/Rules.js
+++ src/Rules.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 const Rules = (props) => {
-
-  const rules=props.rules;
+  const rules = props.rules;
 
   return (
     <div className="w-100">
@@ -13,38 +12,70 @@ const Rules = (props) => {
           alignItems: "center",
           textDecorationLine: "underline",
           marginTop: "3%",
-          marginBottom:"3%",
+          marginBottom: "3%",
         }}
       >
         Rules for reading
       </h2>
-      <div> 
-
-        <div style={{ float: "left", width: "33%", height:450, backgroundColor: "#abe2c7" }}>
-          <h4 className="d-flex justify-content-center pt-3">{rules[0].title}</h4>
+      <div>
+        <div
+          style={{
+            float: "left",
+            width: "33%",
+            height: 450,
+            backgroundColor: "#abe2c7",
+          }}
+        >
+          <h4 className="d-flex justify-content-center pt-3">
+            {rules[0].title}
+          </h4>
           {rules[0].item.map(({ item_id, item_title }, index) => {
-              return (
-          <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>{item_id+1}. {item_title}</p>
+            return (
+              <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>
+                {item_id + 1}. {item_title}
+              </p>
             );
-            })}
+          })}
         </div>
 
-        <div style={{ float: "left", width: "33%", height:450, backgroundColor: "#d6fadf" }}>
-          <h4 className="d-flex justify-content-center pt-3">{rules[1].title}</h4>
+        <div
+          style={{
+            float: "left",
+            width: "33%",
+            height: 450,
+            backgroundColor: "#d6fadf",
+          }}
+        >
+          <h4 className="d-flex justify-content-center pt-3">
+            {rules[1].title}
+          </h4>
           {rules[1].item.map(({ item_id, item_title }, index) => {
-              return (
-          <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>{item_id+1}. {item_title}</p>
+            return (
+              <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>
+                {item_id + 1}. {item_title}
+              </p>
             );
-            })}
+          })}
         </div>
 
-        <div style={{ float: "left", width: "33%",height:450, backgroundColor: "#91c9d6" }}>
-          <h4 className="d-flex justify-content-center pt-3">{rules[2].title}</h4>
+        <div
+          style={{
+            float: "left",
+            width: "33%",
+            height: 450,
+            backgroundColor: "#91c9d6",
+          }}
+        >
+          <h4 className="d-flex justify-content-center pt-3">
+            {rules[2].title}
+          </h4>
           {rules[2].item.map(({ item_id, item_title }, index) => {
-              return (
-          <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>{item_id+1}. {item_title}</p>
+            return (
+              <p style={{ paddingLeft: "5%", paddingRight: "5%" }}>
+                {item_id + 1}. {item_title}
+              </p>
             );
-            })}
+          })}
         </div>
       </div>
     </div>
