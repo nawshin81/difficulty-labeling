8346e6924033d2b127a81450297605aff33fcaff
nawshin81 <nawshinulfat@iut-dhaka.edu>
2021-12-21 21:23:50 +0600

    bug fixed

--- difficulty-labeling-Api/src/models/index.js
+++ difficulty-labeling-Api/src/models/index.js
@@ -11,6 +11,12 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 		acquire: dbConfig.pool.acquire,
 		idle: dbConfig.pool.idle,
 	},
+	dialectOptions: {
+		options: {
+		encrypt: false,
+		},
+	},
+	
 });
 
 const db = {};
